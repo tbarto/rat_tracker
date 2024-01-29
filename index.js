af735abf32f32e9c374847a05a237b5d0a928822
Tom Barto <tombarto@Toms-MacBook-Air.local>
2024-01-29 05:46:03 -0500

    fixed heatmap loading

--- index.js
+++ index.js
@@ -9,21 +9,15 @@
 // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=visualization">
 let map, heatmap, pointArray;
 
-
-
 function initMap() {
 
-	pointArray =  pointArray = new google.maps.MVCArray(getPoints());
+	const points = getPoints()
 
 	map = new google.maps.Map(document.getElementById("map"), {
 		zoom: 13,
 		center: { lat: 42.3601, lng: -71.0589 },
 		mapTypeId: "satellite",
 	});
-	heatmap = new google.maps.visualization.HeatmapLayer({
-		data: pointArray
-	});
-	heatmap.setMap(map);
 	map.addListener("click", addPoint);
 }
 
@@ -49,25 +43,14 @@ function addPoint(e) {
 			return response.json()
 		})
 		.then(data => {
-			console.log("new data point: ")
-			console.log(data.latitude)
-			console.log(data.longitude)
 			// Create a new point with the lat and long from click event
-			const point = new google.maps.LatLng(data.latitude, data.longitude)
-			// Add the new point to the heatmap
-			// heatmap.data.push(point)
-			pointArray.push(point)
-			console.log(pointArray.length)
-			google.maps.event.trigger(map, "resize");
+			const point = new google.maps.LatLng(data.latitude, data.longitude);
+			pointArray.push(point);
 		})
 		.catch((error) => {
 			console.log(error)
 		});
 
-}
-
-function toggleHeatmap() {
-	heatmap.setMap(heatmap.getMap() ? null : map);
 }
 
 function getPoints() {
@@ -89,16 +72,22 @@ function getPoints() {
 			return response.json()
 		})
 		.then(data => {
-			console.log(data)
 			for (let i in data) {
 				let newPoint = new google.maps.LatLng(data[i]["latitude"], data[i]["longitude"])
 				dataPoints.push(newPoint)
 			}
+			pointArray = new google.maps.MVCArray(dataPoints);
+			heatmap = new google.maps.visualization.HeatmapLayer({
+				data: pointArray,
+				map: map			
+			});
+			heatmap.set("radius", 40)
+			heatmap.set("opacity", .9)
 		})
 		.catch((error) => {
 			console.log(error)
 		});
-
+	
 	return dataPoints
 }
 
